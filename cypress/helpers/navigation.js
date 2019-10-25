a463759310643e6c89d0b892d85f370938064fe6
Daniel Sanchez <daniel.edw.sanchez@gmail.com>
2019-10-25 15:03:25 -0400

    Navigation helper class

---
+++ cypress/helpers/navigation.js
@@ -0,0 +1,6 @@
+export class NavigationLinks {
+
+    // Add locators for standard navigation links for .primary-nav .secondary-nav
+    // Instead of having same locators in different page objects
+
+}
